74a86660c45718a1bf4d60ac85d6763414471e07
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2024-01-04 20:25:51 +0530

    update code input page to accept user input

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+PLACEHOLDER_CODE = "print('Hello, World!')"
+
 @app.route("/", methods=["GET"])
 def code():
     context = {
         "message": "Paste Your Python Code",
+        "code": PLACEHOLDER_CODE,
     }
     return render_template("code_input.html", **context)
