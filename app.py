3c69fefdc0fc9c2b0d59868b5a8d62f9a5f6b2a0
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2024-01-04 22:10:06 +0530

    styling the code input page

--- app.py
+++ app.py
@@ -1,15 +1,39 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import (
+    Flask,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
 
 app = Flask(__name__)
+app.secret_key = "2e9ac41b1e0b66a8d93d66400e2300c4b4c2953f"
 
 PLACEHOLDER_CODE = "print('Hello, World!')"
 
 @app.route("/", methods=["GET"])
 def code():
+    if session.get("code") is None:
+        session["code"] = PLACEHOLDER_CODE
+    lines = session["code"].split("\n")
     context = {
         "message": "Paste Your Python Code",
-        "code": PLACEHOLDER_CODE,
+        "code": session["code"],
+        "num_lines": len(lines),
+        "max_chars": len(max(lines, key=len)),
     }
     return render_template("code_input.html", **context)
+
+@app.route("/save_code", methods=["POST"])
+def save_code():
+    session["code"] = request.form.get("code")
+    return redirect(url_for("code"))
+
+@app.route("/reset_session", methods=["POST"])
+def reset_session():
+    session.clear()
+    session["code"] = PLACEHOLDER_CODE
+    return redirect(url_for("code"))
