57c5753182e1c185b8edb11da3a9253761104eb1
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2024-01-05 15:08:18 +0530

    introduce syntax highlighting with Pygments

--- app.py
+++ app.py
@@ -8,11 +8,16 @@ from flask import (
     session,
     url_for,
 )
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import Python3Lexer
+from pygments.styles import get_all_styles
 
 app = Flask(__name__)
 app.secret_key = "2e9ac41b1e0b66a8d93d66400e2300c4b4c2953f"
 
 PLACEHOLDER_CODE = "print('Hello, World!')"
+DEFAULT_STYLE = "monokai"
 
 @app.route("/", methods=["GET"])
 def code():
@@ -37,3 +42,24 @@ def reset_session():
     session.clear()
     session["code"] = PLACEHOLDER_CODE
     return redirect(url_for("code"))
+
+@app.route("/style", methods=["GET"])
+def style():
+    if session.get("style") is None:
+        session["style"] = DEFAULT_STYLE
+    formatter = HtmlFormatter(style=session["style"])
+    context = {
+        "message": "Select Your Style 🎨",
+        "all_styles": list(get_all_styles()),
+        "style_definitions": formatter.get_style_defs(),
+        "style_bg_color": formatter.style.background_color,
+        "highlighted_code": highlight(
+            session["code"], Python3Lexer(), formatter
+        ),
+    }
+    return render_template("style_selection.html", **context)
+
+@app.route("/save_style", methods=["POST"])
+def save_style():
+    session["style"] = request.form.get("style")
+    return redirect(url_for("style"))
